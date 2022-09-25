7e4e94fb3f0320d5dd3b9fd10f89eef882ab6013
Mohamed Salah <mosalah@Mohameds-MacBook-Air.local>
2022-09-25 20:55:50 +0200

    code refactor

--- create-stack.sh
+++ create-stack.sh
@@ -1,5 +1,6 @@
 aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://webapp-template.yaml \
---parameters file://parameters.yaml \
---region=us-east-1
+--parameters file://parameters.json \
+--region=us-east-1 \
+--capabilities CAPABILITY_NAMED_IAM
